53b6ee8a59a87986a011014a88edae24a8aa3fc8
Cloud <4gw1x@mail.ru>
2024-02-11 02:36:44 +0300

    fix theme, retrit

---
+++ js/zagotobki.js
@@ -0,0 +1,9 @@
+
+var dropdownContent = document.querySelector('.dropdown-content');
+var scrollWidth = dropdownContent.scrollWidth;
+
+dropdownContent.addEventListener('wheel', function(event) {
+  event.preventDefault();
+  dropdownContent.scrollLeft += event.deltaY;
+
+});
